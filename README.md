f85f96dc3315844836c7c2e9ce128a80404a85c5
Dominik Fuchß <fuchss@escde.net>
2020-04-29 16:47:01 +0200

    Added LUIS entity extraction. Written more details in README.

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Custom Entity Extraction for RASA
-This repository contains a custom entity extractor for RASA ...
+This repository contains some custom entity extractors for RASA. Further details will follow below.
+
+## Database Entity Extractor
+An entity extractor for [MySQL](https://www.mysql.com/). You can simply use the a database to extract entities via fuzzy sets. You have to define queries for the different entity types.
+
+A sample config for the extractor is [here](rasa_cer_config_sample_db.json):
+
+```json
+{
+  "database_config": {
+    "host": "<host_ip_address>",
+    "user": "<database_user>",
+    "password": "<database_user_pw>",
+    "database": "<database_name>"
+  },
+  "database_queries": {
+    "firstnames": "SELECT name FROM NamesDB WHERE EntityType = 'firstname';",
+    "lastnames": "SELECT name FROM NamesDB WHERE EntityType = 'lastname';"
+  },
+  "minimumConfidence": 0.81
+}
+```
+
+A sample config for RASA could look like [here](rasa_config_sample_db.yml)
+```yml
+language: en
+pipeline:
+  - name: WhitespaceTokenizer
+  - name: RegexFeaturizer
+  - name: database_entity_extractor.DatabaseEntityExtractor
+    config: "/path/to/sample_config.json"
+```
+
+## LUIS Entity Extractor
+An entity extractor for [LUIS](https://www.luis.ai). You can simply use the exported LUIS model to extract entities via fuzzy sets. Currently only list entities are supported.
+
+A sample config for RASA:
+
+```yml
+language: en
+pipeline:
+  - name: WhitespaceTokenizer
+  - name: RegexFeaturizer
+  - name: luis_entity_extractor.LuisEntityExtractor
+    config: "luis.json"
+    min_confidence: 0.8
+```
---
+++ luis_entity_extractor.py
@@ -0,0 +1,91 @@
+import json
+from typing import Any, Dict, Optional, Text
+
+import rasa.utils.io
+from rasa.nlu.config import RasaNLUModelConfig
+from rasa.nlu.extractors import EntityExtractor
+from rasa.nlu.model import Metadata
+from rasa.nlu.training_data import Message, TrainingData
+
+try:
+    from cfuzzyset import cFuzzySet as FuzzySet
+except ImportError:
+    from fuzzyset import FuzzySet
+
+
+class LuisEntityExtractor(EntityExtractor):
+    """
+    This is a custom entity extractor accessing a LUIS configuration file that contains a list of entities.
+    This class performs (fuzzy-)matching of an input against every known list entity. The most similar entities
+    are returned as entities.
+    """
+
+    name = "LuisEntityExtractor"
+    provides = ["entities"]
+    requires = ["tokens"]
+
+    def __init__(self, parameters: Dict[Text, Text]) -> None:
+        super(LuisEntityExtractor, self).__init__(parameters)
+
+        if parameters is None:
+            raise AttributeError("No valid config given!")
+        if not isinstance(parameters, dict):
+            raise AttributeError(f"config has type {type(parameters)}")
+        if "config" not in parameters.keys():
+            raise AttributeError(f"config not given: parameters contains {parameters.keys()}")
+
+        with open(parameters["config"]) as json_file:
+            parsed = json.load(json_file)
+        self._entities = self._load(parsed["closedLists"])
+
+        self._min_confidence = 0.7 if "min_confidence" not in parameters.keys() else parameters["min_confidence"]
+
+    def process(self, message: Message, **kwargs: Any) -> None:
+        """
+        Process an incoming message by determining the most similar (or matching) names.
+        """
+        extracted = self._match_entities(message)
+        message.set("entities", message.get("entities", []) + extracted, add_to_output=True)
+
+    def _load(self, parsed_entities):
+        entities = []
+        for group in parsed_entities:
+            group_name = group["name"]
+            for group_element in group["subLists"]:
+                fuzzy = FuzzySet()
+                for x in [group_element["canonicalForm"]] + group_element["list"]:
+                    fuzzy.add(x)
+
+                entity = {
+                    "group": group_name,
+                    "canonical": group_element["canonicalForm"],
+                    "fuzzy": fuzzy
+                }
+                entities.append(entity)
+        return entities
+
+    def _match_entities(self, message: Message):
+        """
+        Perform fuzzy matching on each token of the message.
+        A token contains its text, its offset, its end and optionally additional data.
+        """
+        extracted_entities = []
+        tokens = message.get("tokens")
+        for token in tokens:
+            for entity in self._entities:
+                matches = entity["fuzzy"].get(token.text)
+
+                if matches is None:
+                    continue
+                for match in matches:
+                    if match[0] < self._min_confidence:
+                        continue
+                    match = {
+                        "start": token.offset,
+                        "end": token.end,
+                        "value": entity["canonical"],
+                        "confidence": match[0],
+                        "entity": entity["group"],
+                    }
+                    extracted_entities.append(match)
+        return extracted_entities
